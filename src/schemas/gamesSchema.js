841ad96b6680ed018ab77b5aa8e23c0db1fde206
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-11-04 13:50:38 -0300

    mensajes de error personalizados

--- src/schemas/gamesSchema.js
+++ src/schemas/gamesSchema.js
@@ -1,10 +1,22 @@
 import yup from 'yup';
 
+const errorMsgNameString = 'El nombre del juego debe ser un string';
+const errorMsgNameRequired = 'El nombre del juego es requerido';
+
+const errorMsgGenreString = 'El género del juego debe ser un array';
+const errorMsgGenreRequired = 'El género del juego es requerido';
+
+const errorMsgMembersArray = 'Los miembros del juego deben ser un array';
+const errorMsgMembersRequired = 'Los miembros del juego son requeridos';
+
+const errorMsgEditionString = 'La edición del juego debe ser un string';
+const errorMsgEditionRequired = 'La edición del juego es requerida';
+
 export const addGameSchema = yup.object(
     {
-        name: yup.string().required(),
-        genre: yup.string().required(),
-        members: yup.array().required(),
-        edition: yup.string().required()
+        name: yup.string(errorMsgNameString).required(errorMsgNameRequired),
+        genre: yup.string(errorMsgGenreString).required(errorMsgGenreRequired),
+        members: yup.array(errorMsgMembersArray).required(errorMsgMembersRequired),
+        edition: yup.string(errorMsgEditionString).required(errorMsgEditionRequired)
     }
 );
--- src/schemas/votesSchema.js
+++ src/schemas/votesSchema.js
@@ -1,15 +1,48 @@
 import yup from 'yup';
 
+const errorMsgJudgeString = 'El id del juez debe ser un string';
+const errorMsgJudgeRequired = 'El id del juez es requerido';
+
+const errorMsgGameString = 'El id del juego debe ser un string';
+const errorMsgGameRequired = 'El id del juego es requerido';
+
+const errorMsgJudgeNameString = 'El nombre del juez debe ser un string';
+const errorMsgJudgeNameRequired = 'El nombre del juez es requerido';
+
+const errorMsgGameNameString = 'El nombre del juego debe ser un string';
+const errorMsgGameNameRequired = 'El nombre del juego es requerido';
+
+const errorMsgJugabilidadScoreNumber = 'El puntaje de jugabilidad debe ser un número';
+const errorMsgJugabilidadScoreRequired = 'El puntaje de jugabilidad es requerido';
+const errorMsgJugabilidadScoreMin = 'El puntaje de jugabilidad debe ser mayor a 0';
+const errorMsgJugabilidadScoreMax = 'El puntaje de jugabilidad debe ser menor a 10';
+
+const errorMsgArteScoreNumber = 'El puntaje de arte debe ser un número';
+const errorMsgArteScoreRequired = 'El puntaje de arte es requerido';
+const errorMsgArteScoreMin = 'El puntaje de arte debe ser mayor a 0';
+const errorMsgArteScoreMax = 'El puntaje de arte debe ser menor a 10';
+
+const errorMsgSonidoScoreNumber = 'El puntaje de sonido debe ser un número';
+const errorMsgSonidoScoreRequired = 'El puntaje de sonido es requerido';
+const errorMsgSonidoScoreMin = 'El puntaje de sonido debe ser mayor a 0';
+const errorMsgSonidoScoreMax = 'El puntaje de sonido debe ser menor a 10';
+
+const errorMsgAfinidadScoreNumber = 'El puntaje de afinidad debe ser un número';
+const errorMsgAfinidadScoreRequired = 'El puntaje de afinidad es requerido';
+const errorMsgAfinidadScoreMin = 'El puntaje de afinidad debe ser mayor a 0';
+const errorMsgAfinidadScoreMax = 'El puntaje de afinidad debe ser menor a 10';
+
+
 export const toVoteCreateSchema = yup.object(
     {
-        judge_id: yup.string().required(),
-        game_id: yup.string().required(),
-        judge_name: yup.string().required(),
-        game_name: yup.string().required(),
-        jugabilidad_score: yup.number().required(),
-        arte_score: yup.number().integer().positive().min(1).max(10).required(),
-        sonido_score: yup.number().integer().positive().min(1).max(10).required(),
-        afinidad_score: yup.number().integer().positive().min(1).max(10).required()
+        judge_id: yup.string(errorMsgJudgeString).required(errorMsgJudgeRequired),
+        game_id: yup.string(errorMsgGameString).required(errorMsgGameRequired),
+        judge_name: yup.string(errorMsgJudgeNameString).required(errorMsgJudgeNameRequired),
+        game_name: yup.string(errorMsgGameNameString).required(errorMsgGameNameRequired),
+        jugabilidad_score: yup.number(errorMsgJugabilidadScoreNumber).integer().positive().min(1, errorMsgJugabilidadScoreMin).max(10, errorMsgJugabilidadScoreMax).required(errorMsgJugabilidadScoreRequired),
+        arte_score: yup.number(errorMsgArteScoreNumber).integer().positive().min(1,errorMsgArteScoreMin).max(10, errorMsgArteScoreMax).required(errorMsgArteScoreRequired),
+        sonido_score: yup.number(errorMsgSonidoScoreNumber).integer().positive().min(1, errorMsgSonidoScoreMin).max(10, errorMsgSonidoScoreMax).required(errorMsgSonidoScoreRequired),
+        afinidad_score: yup.number(errorMsgAfinidadScoreNumber).integer().positive().min(1, errorMsgAfinidadScoreMin).max(10, errorMsgAfinidadScoreMax).required(errorMsgAfinidadScoreRequired)
     }
 );
 
