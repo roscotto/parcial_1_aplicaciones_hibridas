d0ab1fa685a4beaec8e5b028a2c1e2a69098f8ab
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-11-26 15:12:57 -0300

    instalación de cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import gamesRoutes from './routes/gamesRoutes.js'
 import judgesRoutes from './routes/judgesRoutes.js'
+import cors from 'cors'
 
-
+app.use(cors()) // permite que cualquier persona se conecte a mi api
 const app = express()
 
 app.use(express.json()) // interpreta el body cuando viene un json
